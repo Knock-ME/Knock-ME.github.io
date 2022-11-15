51feb02f62361a711a912e5c2e02e43d788c5bac
Yamin Mahdi <yamin_khan@asia.com>
2022-11-15 16:10:11 +0600

    ..

--- script/login.js
+++ script/login.js
@@ -22,41 +22,44 @@ window.fbAsyncInit = function () {
 
 
 
-function checkLoginState() 
-{
-    FB.login(function (response) {
-        if (response.authResponse) {
-            console.log('Welcome!  Fetching your information.... ');
-            FB.api('/me', function (response) {
-                console.log('id, ' + response.id + '.');
-                console.log('Good to see you, ' + response.name + '.');
-                sessionStorage.setItem("id",response.id);
-                sessionStorage.setItem("nm",response.name);
-            });
-            // FB.api(
-            //     '/me/picture',
-            //     'GET',
-            //     {},
-            //     function(response) {
-            //         // Insert your code here
-            //         sessionStorage.setItem("pic",response.data.url);
-            //         console.log('id, ' +response.data.url + '.');
-
-            //     }
-            //   );
-        } else {
+function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function (response) {   // See the onlogin handler
+        if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+            testAPI();
+        } else {                                 // Not logged into your webpage or we are unable to tell.
             console.log('User cancelled login or did not fully authorize.');
+            FB.login(function (response) {
+                if (response.authResponse) {
+                    testAPI();
+                } else {
+                    console.log('User cancelled login or did not fully authorize.');
+                }
+            });
         }
     });
-    // FB.getLoginStatus(function (response) 
-    // {
-    //     if (response.status === 'connected') 
-    //     {
-    //         var accessToken = response.authResponse.accessToken;
+}
 
-    //     }
-    // });
-        
+function testAPI() {
+
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function (response) {
+        console.log('id, ' + response.id + '.');
+        console.log('Good to see you, ' + response.name + '.');
+        sessionStorage.setItem("id", response.id);
+        sessionStorage.setItem("nm", response.name);
+    });
+    FB.api(
+        '/me/picture',
+        'GET',
+        {},
+        function (response) {
+            // Insert your code here
+            sessionStorage.setItem("pic", response.data.url);
+            console.log('id, ' + response.data.url + '.');
+
+        }
+    );
+
 }
 
 
