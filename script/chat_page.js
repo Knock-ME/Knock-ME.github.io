2fdaae2f2f3ddc08034efbbf316277306c53f886
Yamin Mahdi <yamin_khan@asia.com>
2022-11-17 22:39:36 +0600

    Update chat_page.js

--- script/chat_page.js
+++ script/chat_page.js
@@ -288,7 +288,26 @@ on('click', '.sendBtn', function (e)
       {
       //uploading blob to firebase storage
       const uploadTask = uploadBytesResumable(storageRef, bolb);
-      uploadTask.on('state_changed', ()=> 
+      uploadTask.on('state_changed', 
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      }, 
+      (error) => {
+        // Handle unsuccessful uploads
+        console.error(error);
+      },   
+      ()=> 
         {
           // Handle successful uploads on complete
           // For instance, get the download URL: https://firebasestorage.googleapis.com/...
@@ -299,6 +318,8 @@ on('click', '.sendBtn', function (e)
             sendMsg(msg);
             });
         });
+      }).catch(error => {
+        console.error(error);
       });
 
   //msg.pic=currentConfig.pic;
